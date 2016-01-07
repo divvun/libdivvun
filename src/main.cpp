5b0f30e38132beb887af2891de118e0982052586
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-01-07 15:29:54 +0100

    cg works

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+bool VERBOSE=false;
+
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
@@ -101,6 +103,48 @@ const StringVec split(const std::string str, const char delim=' ')
 	return tokens;
 }
 
+StringVec cg_keywords = {
+	std::string("ADD:"),
+	std::string("MAP:"),
+	std::string("REPLACE:"),
+	std::string("SELECT:"),
+	std::string("REMOVE:"),
+	std::string("IFF:"),
+	std::string("APPEND:"),
+	std::string("SUBSTITUTE:"),
+	std::string("REMVARIABLE:"),
+	std::string("SETVARIABLE:"),
+	std::string("DELIMIT:"),
+	std::string("MATCH:"),
+	std::string("SETPARENT:"),
+	std::string("SETCHILD:"),
+	std::string("ADDRELATION:"),
+	std::string("SETRELATION:"),
+	std::string("REMRELATION:"),
+	std::string("ADDRELATIONS:"),
+	std::string("SETRELATIONS:"),
+	std::string("REMRELATIONS:"),
+	std::string("MOVE:"),
+	std::string("MOVE-AFTER:"),
+	std::string("MOVE-BEFORE:"),
+	std::string("SWITCH:"),
+	std::string("REMCOHORT:"),
+	std::string("UNMAP:"),
+	std::string("COPY:"),
+	std::string("ADDCOHORT:"),
+	std::string("ADDCOHORT-AFTER:"),
+	std::string("ADDCOHORT-BEFORE:"),
+	std::string("EXTERNAL:"),
+	std::string("EXTERNAL-ONCE:"),
+	std::string("EXTERNAL-ALWAYS:"),
+	std::string("REOPEN-MAPPINGS:"),
+};
+
+inline int startswith(std::string big, std::string start)
+{
+	return big.compare(0, start.size(), start) == 0;
+}
+
 int main(int argc, char ** argv)
 {
 	if (argc != 2) {
@@ -111,29 +155,51 @@ int main(int argc, char ** argv)
 		std::cerr<<std::endl;
 		return(EXIT_FAILURE);
 	}
-	std::cerr <<"Reading transducer "<<argv[1]<<std::endl;
+	if(VERBOSE) {
+		std::cerr <<"Reading transducer "<<argv[1]<<std::endl;
+	}
 	const hfst::HfstTransducer *t = readTransducer(argv[1]);
 
 	std::string wf;
 	for (std::string line; std::getline(std::cin, line);) {
+		std::cout << line << std::endl;
 		if(line.size()>2 && line[0]=='"' && line[1]=='<') {
 			wf = line;
-			std::cout <<wf << std::endl;
 		}
 		else if(line.size()>2 && line[0]=='\t' && line[1]=='"') {
 			// TODO: doesn't do anything with subreadings yet; needs to keep track of previous line(s) for that
 			int lemma_end = line.find("\" ");
 			std::string lemma = line.substr(2, lemma_end-2);
 			std::string tags = line.substr(lemma_end+2);
-			// split removes empties; TODO: only keep certain tags:
-			auto tagsplus = join(split(tags), "+");
-			std::cout << line << std::endl;
+			StringVec gentags;
+			bool suggest = false;
+			for(auto& tag : split(tags)) {
+				if(tag == "&SUGGEST") {
+					suggest=true;
+				}
+				if(tag.size()>0 && !(tag[0]=='#' ||
+						     tag[0]=='&' ||
+						     tag[0]=='@' ||
+						     tag[0]=='<' ||
+						     startswith(tag, "SUBSTITUTE:") ||
+						     startswith(tag, "COPY:") ||
+						     startswith(tag, "ADD:") ||
+						     startswith(tag, "MAP:")
+					   )) {
+					gentags.push_back(tag);
+				}
+			}
+			if(!suggest) {
+				continue;
+			}
+			auto tagsplus = join(gentags, "+");
 			auto ana = lemma+"+"+tagsplus;
 			auto paths = t->lookup_fd({ ana }, -1, 10.0);
+			std::cout << ana<<"\t";
 			if(paths->size() > 0) {
 				for(auto& p : *paths) {
 					for(auto& symbol : p.second) {
-						// TODO: this is a hack to avoid flag diacritics, is there a way to make lookup skip them?
+						// TODO: this is a hack to avoid flag diacritics; is there a way to make lookup skip them?
 						if(symbol.size()>0 && symbol[0]!='@') {
 							std::cout << symbol;
 						}
@@ -147,8 +213,7 @@ int main(int argc, char ** argv)
 			}
 		}
 		else {
-			std::cout << line << std::endl;
+			//pass
 		}
 	}
-	std::cout << "EOF" << std::endl;
 }
