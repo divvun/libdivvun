20efaaea47b19c146dc3cadef7fabcf1a09df5a7
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2017-03-16 13:10:32 +0100

    install dtd and dtd-validation script
    (and generated .rnc for emacs)

---
+++ src/validate-header.sh
@@ -0,0 +1,14 @@
+# -*- sh-shell: bash -*-
+
+if [[ $# != 1 ]]; then
+    echo "USAGE: $(basename "$0") <input_file>"
+    exit 1
+fi
+
+FILE1=$1
+
+if [[ ! -e $FILE1 ]]; then
+    echo "ERROR: '$1' file not found"
+    exit 1
+fi
+
