145e63c4a3860de304e218b6f00f40ccecf0f681
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2018-01-23 13:58:31 +0100

    (C) 2018

--- src/blanktag.cpp
+++ src/blanktag.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2015-2017, Kevin Brubeck Unhammer <unhammer@fsfe.org>
+* Copyright (C) 2015-2018, Kevin Brubeck Unhammer <unhammer@fsfe.org>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
